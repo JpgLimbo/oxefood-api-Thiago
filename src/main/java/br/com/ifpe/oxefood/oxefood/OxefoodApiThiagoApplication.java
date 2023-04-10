a083acf6e826c29d79b38b1c534f4d6d0e605f49
= <=>
2023-04-10 15:18:56 -0300

    alterando o diretorio oxefood

--- src/main/java/br/com/ifpe/oxefood/oxefood/OxefoodApiThiagoApplication.java
+++ src/main/java/br/com/ifpe/oxefood/oxefood/OxefoodApiThiagoApplication.java
@@ -1,4 +1,4 @@
-package br.com.ifpe;
+package br.com.ifpe.oxefood.oxefood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
