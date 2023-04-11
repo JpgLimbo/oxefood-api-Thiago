49f0481d5a3949a7ee1d8c65de489e45ddf5bc8b
= <=>
2023-04-11 14:00:14 -0300

    Corrigindo, agora ta tudo certo

--- src/main/java/br/com/ifpe/oxefood/OxefoodApiThiagoApplication.java
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiThiagoApplication.java
@@ -1,4 +1,4 @@
-package br.com.ifpe.oxefood.oxefood;
+package br.com.ifpe.oxefood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
