9a386174edd12dcc6f35381afb7c4934cb0e3917
= <=>
2023-05-22 15:10:33 -0300

    implementando botão editar

--- src/main/java/br/com/ifpe/oxefood/modelo/comprador/CompradorService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/comprador/CompradorService.java
@@ -7,7 +7,6 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.com.ifpe.oxefood.modelo.cliente.Cliente;
 import br.com.ifpe.oxefood.util.entity.GenericService;
 
 @Service
@@ -23,20 +22,7 @@ public class CompradorService extends GenericService {
        return repository.save(comprador);
    }
 
-   @Transactional
-   public void update(Long id, Cliente clienteAlterado) {
-
-      Cliente cliente = repository.findById(id).get();
-      cliente.setNome(clienteAlterado.getNome());
-      cliente.setDataNascimento(clienteAlterado.getDataNascimento());
-      cliente.setCpf(clienteAlterado.getCpf());
-      cliente.setFoneCelular(clienteAlterado.getFoneCelular());
-      cliente.setFoneFixo(clienteAlterado.getFoneFixo());
-	    
-      super.preencherCamposAuditoria(cliente);
-      repository.save(cliente);
-  }
-
+   
    public List<Comprador> listarTodos() {
   
     return repository.findAll();
