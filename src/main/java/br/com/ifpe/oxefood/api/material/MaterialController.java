edb2bfaa2a0808377f2c06f7e4d96038e7554910
= <=>
2023-05-31 16:40:49 -0300

    01

--- src/main/java/br/com/ifpe/oxefood/api/material/MaterialController.java
+++ src/main/java/br/com/ifpe/oxefood/api/material/MaterialController.java
@@ -7,9 +7,11 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,5 +44,18 @@ public class MaterialController extends GenericController {
    public Material obterPorID(@PathVariable Long id) {
 
        return materialService.obterPorID(id);
+   }
+   @PutMapping("/{id}")
+   public ResponseEntity<Material> update(@PathVariable("id") Long id, @RequestBody MaterialRequest request) {
+
+       materialService.update(id, request.build());
+       return ResponseEntity.ok().build();
+   }
+
+   @DeleteMapping("/{id}")
+   public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+       materialService.delete(id);
+       return ResponseEntity.ok().build();
    }
 }
